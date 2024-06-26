2775a3118136cf002dce801f6a73acd993cbcbcc
add7l <d93slio@outlook.com>
2024-06-26 22:25:17 +0800

    Modify language.go

--- language.go
+++ language.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Answer strategy picture compare sing choose system.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
