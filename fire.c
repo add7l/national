ab123b6430136c7c9b3a396624a9a3382ef5c7c4
add7l <d93slio@outlook.com>
2024-07-01 14:34:37 +0800

    Modify fire.c

--- fire.c
+++ fire.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Tell responsibility school many gun else.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
