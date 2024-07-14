4cfc2d70f3f25151573b204b42d2078e3a033ffe
add7l <d93slio@outlook.com>
2024-07-14 15:16:19 +0800

    Modify offer.cpp

--- offer.cpp
+++ offer.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# View color image board box tend.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
