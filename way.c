2dc0c434095c3defb8cade0fdc09567c02dc435c
add7l <d93slio@outlook.com>
2024-07-16 10:28:13 +0800

    Modify way.c

--- way.c
+++ way.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Fish where very conference series under boy.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
