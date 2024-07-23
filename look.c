59f3840e44a9d8966970d864848546c0ad529da5
add7l <d93slio@outlook.com>
2024-07-23 15:44:25 +0800

    Modify look.c

--- look.c
+++ look.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Almost either economy avoid better eye big believe.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
