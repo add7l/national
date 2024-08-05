d4358986893887859c8cb9066ed245b743d352d9
add7l <d93slio@outlook.com>
2024-08-05 23:01:05 +0800

    Modify many.cpp

--- many.cpp
+++ many.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Star activity save key specific.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
