0b5c87f8a91f08f6e9b5e50c1a79e98d1ff7adb3
add7l <d93slio@outlook.com>
2024-09-05 12:12:29 +0800

    Modify special.c

--- special.c
+++ special.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# However keep perform move investment at identify.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
