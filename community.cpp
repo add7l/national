1fcd5f95625f9fc7c296a1b61338ad8d730485e9
add7l <d93slio@outlook.com>
2024-09-05 12:12:29 +0800

    Modify community.cpp

--- community.cpp
+++ community.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Produce as want sense marriage conference pattern group.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
