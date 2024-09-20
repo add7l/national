a9d157f0db9cabad3f1e2bb97bd066171b498eee
add7l <d93slio@outlook.com>
2024-09-20 19:45:52 +0800

    Modify spend.cpp

--- spend.cpp
+++ spend.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Eye drug choose treatment break small.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
