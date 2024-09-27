612133e266bb42c0826902b493dea7f2623e5821
add7l <d93slio@outlook.com>
2024-09-27 12:48:05 +0800

    Modify color.go

--- color.go
+++ color.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Land drive day until visit small build.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
