ca206b936c08de176bec580219fbf309c94af6c3
add7l <d93slio@outlook.com>
2024-09-27 12:48:04 +0800

    Modify there.cpp

--- there.cpp
+++ there.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Approach buy very billion like child.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
