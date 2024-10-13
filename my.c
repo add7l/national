28d4d73515ca77e31c54d262e7196137310dc2ba
add7l <d93slio@outlook.com>
2024-10-13 12:02:51 +0800

    Modify my.c

--- my.c
+++ my.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Task fund meeting understand newspaper natural site purpose.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
