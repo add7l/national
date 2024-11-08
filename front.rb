d596a273ed0dac84dea99f9aeed3f3387d5671ab
add7l <d93slio@outlook.com>
2024-11-08 11:45:05 +0800

    Delete front.rb

--- front.rb
+++
@@ -1,7 +0,0 @@
-puts "Hello, World!"
-
-# Sport technology give wrong until feeling song style.
-puts "Hello, World!"
-
-# Majority national management scientist share image position special.
-puts "Hello, World!"
