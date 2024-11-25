865dacdc9873256a9db2c59a5ecfcc3db16c0fd3
add7l <d93slio@outlook.com>
2024-11-25 16:42:55 +0800

    Delete join.js

--- join.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# School run rock fund hope action.
-console.log("Hello, World!");
-
-# Peace participant anything daughter break Mrs town executive.
-console.log("Hello, World!");
